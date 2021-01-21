82fb14ea1221a95de7f38973e751ceabf06ba692
SMARTDISK <smartdisk@icloud.com>
2021-01-21 22:13:10 +0900

    working2

--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
 'use strict';
 
-//window.global = window;
-//window.Buffer = require('buffer').Buffer; // browserify
-//const https = require('https');
-//var tiny = require('tiny-json-http')
-
 const pkg = require('../package.json');
 const HOST = pkg.api.host;
 
@@ -15,19 +10,6 @@ var AppRes = /** @class */ (function () {
   }
 
   AppRes.prototype.test = function () {
-    let data = {
-      pkey: "GXYqIgrafjTRatwTB96d",
-      akey: "39f031e6-94a0-4e14-b600-82779ec899d7",
-      cmd: "icon",
-      file: "sample.png"
-    };
-
-    // tiny.post({HOST, data}, (err, res) => {
-    //   console.log("res:" + res);
-    //   console.log("err:" + err);
-
-    // });
-  
     return 'test';
   };
 
