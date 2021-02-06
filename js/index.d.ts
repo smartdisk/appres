8be768cf7a647d632bce534c6b65e9f29d1186c8
SMARTDISK <smartdisk@icloud.com>
2021-02-06 16:01:02 +0900

    String Interface - REST - GET

--- js/index.d.ts
+++ js/index.d.ts
@@ -1,4 +1,8 @@
 export declare class AppRes {
+    static setEnv(host?: any, pkey?: any, akey?: any, lang?: any): void;
+    setEnv(host?: any, pkey?: any, akey?: any, lang?: any): void;
+    static setLang(lang: any): void;
+    setLang(lang: any): void;
     static enableTabKey(id: any): void;
     enableTabKey(id: any): void;  
 }
--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
 'use strict';
 
 const pkg = require('../package.json');
-const axios = require('axios');
 
-const HOST = pkg.api.host;
-const PKEY = "GXYqIgrafjTRatwTB96d";
-const AKEY = "39f031e6-94a0-4e14-b600-82779ec899d7";
-const LANG = "ko-KR";
+let HOST = pkg.api.host;
+let PKEY = "GXYqIgrafjTRatwTB96d";
+let AKEY = "39f031e6-94a0-4e14-b600-82779ec899d7";
+let LANG = "ko-KR";
 
 Object.defineProperty(exports, "__esModule", { value: true });
 var AppRes = /** @class */ (function () {
@@ -17,45 +16,31 @@ var AppRes = /** @class */ (function () {
         super();
         const self = this;
         setTimeout(function(){
-          _string(self.innerText).then((res) => {
-            console.log("res:" + res);
-
-          }, (err) => {
-            console.log("err:" + err);
-
-          });
+          let newtext = null;
+          if(self.hasAttribute('key')) {
+            newtext = window.AppString[self.getAttribute('key')];
+          } else {
+            newtext = window.AppString[self.innerText];
+          }
+          if(newtext) {
+            self.innerText = newtext;  
+          } else {
+            console.log("AppRes:" + self.innerText);
+          }
         }, 0)
       }
     });    
   }
 
-  const _string = (string) => {
-    let data = {
-      pkey: PKEY,
-      akey: AKEY,
-      lang: LANG,
-      cmd: 'string',
-      str: string
-    };
-    let config = {
-      method: 'post',
-      url: HOST,
-      data: data
-    };    
-    return new Promise((resolve, reject) => {
-      axios(config)
-      .then((res) => {
-        resolve(res);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-    });
-}
-
-
-
-
+  function setEnv(host, pkey, akey, lang) {
+    if(host) HOST = host;
+    if(pkey) PKEY = pkey;
+    if(akey) AKEY = akey;
+    if(lang) LANG = lang;
+  }
+  function setLang(lang) {
+    if(lang) LANG = lang;
+  }
   function enableTabKey(id) {
     var el = (typeof id)=="string" ? document.getElementById(id) : id;
     el.onkeydown = function(e) {
@@ -78,12 +63,25 @@ var AppRes = /** @class */ (function () {
         }
     };
   }
-  
+
+  AppRes.prototype.setEnv = function (host, pkey, akey, lang) {
+    setEnv(host, pkey, akey, lang);
+  };
+  AppRes.prototype.setLang = function (lang) {
+    setLang(lang);
+  };  
   AppRes.prototype.enableTabKey = function (id) {
     enableTabKey(id);
   };
 
+
   AppRes.appres = new AppRes();
+  AppRes.setEnv = function(host, pkey, akey, lang) {
+    return this.appres.setEnv(host, pkey, akey, lang);
+  };
+  AppRes.setLang = function(lang) {
+    return this.appres.setLang(lang);
+  };
   AppRes.enableTabKey = function(id) {
     return this.appres.enableTabKey(id);
   };
