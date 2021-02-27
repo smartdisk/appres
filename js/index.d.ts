96c3e6b43385d603920d94f62bdd0d51a5f6ebf5
SMARTDISK <smartdisk@icloud.com>
2021-02-27 17:49:23 +0900

    appres v0.0.15

--- js/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class AppRes {    
-    static setEnv(host?: any, pkey?: any, akey?: any, lang?: any): void;
-    setEnv(host?: any, pkey?: any, akey?: any, lang?: any): void;
-    static setLang(lang: any): void;
-    setLang(lang: any): void;
-
-    static appString(context: any, element: any): string;
-    appString(context: any, element: any): string;
-}
